57e6d1fbffd662227b4ad35a024e72ac13f5aa0a
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-07-10 15:08:41 +0500

    Basic GraphQL Server done

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
-import express from "express"
-import { ApolloServer } from "@apollo/server"
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const books = [
+  {
+    id: 1,
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    id: 2,
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const typeDefs = `#graphql
+
+  type Book {
+    id: Int
+    title: String
+    author: String
+  }
+
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 8000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
