50969540557f850761ccb2c50b46121c5a7834c5
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-07-11 09:05:42 +0500

    use seprate file for schema and resolver

---
+++ GraphQL/Resolvers/Resolver.js
@@ -0,0 +1,12 @@
+import {User} from '../../fakeDB/fakeUser.js'
+import {Post} from '../../fakeDB/fakePost.js'
+export const resolvers = {
+    Query: {
+     getUser:(_,arg)=>User.find(user=>user.id==arg.id),
+     getAllUsers:()=>User,
+     getPost:(_,arg)=>Post.find(post=>post.id==arg.id),
+     getAllPosts:()=>Post
+    },
+    User:{posts:(parent)=>Post.filter(post=>post.userId==parent.id)},
+    Post:{user:(parent)=>User.find(user=>user.id==parent.userId)}
+  };
